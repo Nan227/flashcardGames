a49cbd9a1e9d9163c329c4f843d14d88c7f17a8b
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-03-08 00:42:26 -0500

    update code

---
+++ cardgame/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1> Machine Leaning Flashcard Game</h1>
+      <h2>There are 10 questions without time limit</h2>
+    </div>
+  )
+}
+
+export default App
