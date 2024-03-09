c6d9bd8200e857fe7290721c2e89fd2a896236d4
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-03-08 23:55:31 -0500

    Fontend done!

--- cardgame/src/App.jsx
+++ cardgame/src/App.jsx
@@ -6,8 +6,16 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1> Machine Leaning Flashcard Game</h1>
-      <h2>There are 10 questions without time limit</h2>
+      <h1> Machine Leaning Flashcard</h1>
+      <h2>There are 10 questions. Let's check them out! </h2>
+      <div className ="card">
+        <div className ="question"><h2>Question</h2></div>
+        <div className = "answer">
+          <button className = "btn">Check Answer</button>
+          <button className = "btn">Next Question</button>
+        </div>
+
+      </div>
     </div>
   )
 }
